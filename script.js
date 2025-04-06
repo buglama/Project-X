4dda7108536189ded3674af60755cd2841802439
Farid Humbatov <88279589+fred0128@users.noreply.github.com>
2025-04-06 16:47:12 +0400

    commit 16:4 06.04.2025 Update: frame-4, frame-5 added to home.html page

--- script.js
+++ script.js
@@ -3,5 +3,25 @@ setTimeout(() => {
     setTimeout(() => {
         document.querySelector(".container").style.opacity = "1";
         document.querySelector(".loader").style.display = "none";
-    }, 1000); // Loader tam itəndən sonra container göstərilir
+    }, 1000);
 }, 1000);
+
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("dot");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) { slideIndex = 1 }
+    slides[slideIndex - 1].style.display = "block";
+    setTimeout(showSlides, 2000);
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    dots[slideIndex - 1].className += " active";
+}
