6f0b74e27794b71637e0350bc59698b310eb454f
Farid Humbatov <88279589+fred0128@users.noreply.github.com>
2025-04-09 17:07:48 +0400

    17:07 09.04.2025 Update: QR code added to profit.html page

---
+++ scripts/qr-generator.js
@@ -0,0 +1,20 @@
+let qrData = "Fərid";
+
+const qrCode = new QRCodeStyling({
+    data: qrData,
+    dotsOptions: {
+        color: "#1C304F",
+        type: "rounded"
+    },
+    backgroundOptions: {
+        color: "#98BBF5",
+    },
+    imageOptions: {
+        crossOrigin: "anonymous",
+        margin: 10
+    }
+});
+
+qrCode.append(document.getElementById("qrcode"));
+
+document.getElementById("qr-info").textContent = qrData;
